9d471eb4320ff1e3b2ad6d5cb9942fc9ec24ba0e
Lucas <122295876+K4w4it4chi@users.noreply.github.com>
2023-01-12 10:22:38 -0300

    Add files via upload

---
+++ Adivinhat0r 3000 beta.py
@@ -0,0 +1,29 @@
+print("*******************************************************************************************************")
+print("██████████Senhoras e Senhores sejam muito bem vindo(as) ao Adivinhator 3000 !!██████████")
+print("*******************************************************************************************************")
+
+print("████] 10%")
+print("███████████]40%")
+print("███████████████]60%")
+print("███████████████████]100% Adivinhator Iniciando !")
+
+
+numero_secreto = 777
+
+nickname = input("Nickname:")
+
+
+
+type(nickname)
+
+chute = input("Digite o seu número:89 ")
+
+print("Voce digitou " , chute)
+
+if (numero_secreto  == 777):
+     print("Acertô " +nickname, "A miseravi ")
+else:
+
+     print("Faustão says: EROU")
+
+print("Fim de jogo, aprenda com seus erros")
